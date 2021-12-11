57c7828c6a653c60ad443975365f03f5c3525694
Alexander Gavrilov <angavrilov@gmail.com>
2021-12-11 18:04:34 +0300

    Bone Overlay: support bone wireframe opacity depth fade.
    Add an option that allows fade based on the depth from the camera,
    using exponential decay with the slider specifying the 'half-life'
    depth. This is intended as a way to automatically hide bones
    in distant parts of the mesh while focused on a specific part.

--- source/blender/blenloader/intern/versioning_300.cc
+++ source/blender/blenloader/intern/versioning_300.cc
@@ -3851,5 +3851,20 @@ void blo_do_versions_300(FileData *fd, Library * /*lib*/, Main *bmain)
     LISTBASE_FOREACH (Curves *, curves_id, &bmain->hair_curves) {
       curves_id->flag &= ~CV_SCULPT_SELECTION_ENABLED;
     }
+
+
+    /* Initialize the bone wireframe opacity depth fade setting. */
+    if (!DNA_struct_elem_find(fd->filesdna, "View3DOverlay", "float", "bone_wire_fade_depth")) {
+      LISTBASE_FOREACH (bScreen *, screen, &bmain->screens) {
+        LISTBASE_FOREACH (ScrArea *, area, &screen->areabase) {
+          LISTBASE_FOREACH (SpaceLink *, sl, &area->spacedata) {
+            if (sl->spacetype == SPACE_VIEW3D) {
+              View3D *v3d = (View3D *)sl;
+              v3d->overlay.bone_wire_fade_depth = 1.0f;
+            }
+          }
+        }
+      }
+    }
   }
 }
